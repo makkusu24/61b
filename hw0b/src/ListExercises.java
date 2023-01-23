01ef273b62f96758ed0121b7dfee817fb9feeca6
Max Boston <maxboston@berkeley.edu>
2023-01-22 19:03:31 -0800

    HW0B Final Status

--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -1,25 +1,66 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListExercises {
 
-    /** Returns the total sum in a list of integers */
-	public static int sum(List<Integer> L) {
-        return 0;
+    /**
+     * Returns the total sum in a list of integers
+     */
+    public static int sum(List<Integer> L) {
+        int sum_placeholder = 0;
+        for (int i : L) {
+            sum_placeholder = sum_placeholder + i;
+        }
+        return sum_placeholder;
     }
 
-    /** Returns a list containing the even numbers of the given list */
+    /**
+     * Returns a list containing the even numbers of the given list
+     */
     public static List<Integer> evens(List<Integer> L) {
-        return null;
+        List<Integer> output = new ArrayList<Integer>();
+        for (Integer i : L) {
+            if (i % 2 == 0) {
+                output.add(i);
+            }
+        }
+        return output;
     }
 
-    /** Returns a list containing the common item of the two given lists */
+    /**
+     * Returns a list containing the common item of the two given lists
+     */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
-        return null;
+        List<Integer> common = new ArrayList<Integer>();
+        if (L1.size() < L2.size()) {
+            for (Integer i : L1) {
+                if (L2.contains(i)) {
+                    common.add(i);
+                }
+            }
+        } else {
+            for (Integer j : L2) {
+                if (L1.contains(j)) {
+                    common.add(j);
+                }
+            }
+        }
+        return common;
     }
 
 
-    /** Returns the number of occurrences of the given character in a list of strings. */
+    /**
+     * Returns the number of occurrences of the given character in a list of strings.
+     */
     public static int countOccurrencesOfC(List<String> words, char c) {
-        return 0;
+        Integer count = 0;
+        for (String word : words) {
+            for (char ch : word.toCharArray()) {
+                if (ch == c) {
+                    count ++;
+                }
+            }
+        }
+        return count;
     }
 }
--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,23 +1,49 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MapExercises {
-    /** Returns a map from every lower case letter to the number corresponding to that letter, where 'a' is
+    /**
+     * Returns a map from every lower case letter to the number corresponding to that letter, where 'a' is
      * 1, 'b' is 2, 'c' is 3, ..., 'z' is 26.
      */
     public static Map<Character, Integer> letterToNum() {
-        return null;
+        Character[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        Integer num = 1;
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        for (Character i : letters) {
+            map.put(i, num);
+            num++;
+        }
+        return map;
     }
 
-    /** Returns a map from the integers in the list to their squares. For example, if the input list
-     *  is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to 49.
+    /**
+     * Returns a map from the integers in the list to their squares. For example, if the input list
+     * is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to 49.
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
-        return null;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (Integer i : nums) {
+            map.put(i, i * i);
+        }
+        return map;
     }
 
-    /** Returns a map of the counts of all words that appear in a list of words. */
+    /**
+     * Returns a map of the counts of all words that appear in a list of words.
+     */
     public static Map<String, Integer> countWords(List<String> words) {
-        return null;
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (String word : words) {
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            }
+            else {
+                map.put(word, 1);
+            }
+        }
+        return map;
     }
 }
