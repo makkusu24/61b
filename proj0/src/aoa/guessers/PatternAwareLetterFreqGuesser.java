15ffaf31215653f236937176f1ee426c8c87056c
Max Boston <maxboston@berkeley.edu>
2023-01-25 22:44:53 -0800

    passed both naive and pattern aware

--- proj0/src/aoa/guessers/PatternAwareLetterFreqGuesser.java
+++ proj0/src/aoa/guessers/PatternAwareLetterFreqGuesser.java
@@ -1,6 +1,8 @@
 package aoa.guessers;
 
 import aoa.utils.FileUtils;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -13,9 +15,39 @@ public class PatternAwareLetterFreqGuesser implements Guesser {
     }
 
     /** @Override **/
-    public Map<Character, Integer> getFrequencyMap() {
+    public Map<Character, Integer> getPatternFrequencyMap(String pattern) {
         Map<Character, Integer> freq = new TreeMap<>();
-        for (String word : words) {
+        List<String> patternWords = new ArrayList<>();
+        List<String> patternWords2 = new ArrayList<>();
+        char[] patternArray = pattern.toCharArray();
+        /** the below 2 loops parses the viable words from the rest of the words list**/
+        for (String element : words) {
+            if (element.length() == pattern.length()) {
+                patternWords.add(element);
+            }
+        }
+        for (String element2 : patternWords) {
+            Integer i = 0;
+            for (Character element3 : element2.toCharArray()) {
+                if (patternArray[i] == '-' && i == patternArray.length-1) {
+                    patternWords2.add(element2);
+                }
+                else if (patternArray[i] == '-') {
+                    i++;
+                }
+                else if (i == patternArray.length-1 && patternArray[i] == element3){
+                    patternWords2.add(element2);
+                }
+                else if (patternArray[i] != element3) {
+                    break;
+                }
+                else {
+                    i++;
+                }
+            }
+        }
+        /** loop below finds the most frequent letter from the list of words remaining that match the pattern**/
+        for (String word : patternWords2) {
             for (Character ch : word.toCharArray()) {
                 if (freq.containsKey(ch)) {
                     freq.put(ch, freq.get(ch)+1);
@@ -25,14 +57,15 @@ public class PatternAwareLetterFreqGuesser implements Guesser {
                 }
             }
         }
+        freq.put('z', 0);
         return freq;
     }
     /** Returns the most common letter in the set of valid words based on the current
      *  PATTERN. */
     public char getGuess(String pattern, List<Character> guesses) {
-        Map<Character, Integer> freq = getFrequencyMap();
+        Map<Character, Integer> freq = getPatternFrequencyMap(pattern);
         /** List<Character> notGuesses = new ArrayList<Character>(); **/
-        Character prevMax = '|';
+        Character prevMax = 'z';
         for (Character key : freq.keySet()) {
             if (freq.size() == 0) {
                 return '?';
