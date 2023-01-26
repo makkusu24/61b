aeb7d60e0d60ee9961d8f92c627199b9ea3c8fb8
Max Boston <maxboston@berkeley.edu>
2023-01-25 21:16:53 -0800

    palfg = nalfg

--- proj0/src/aoa/guessers/PatternAwareLetterFreqGuesser.java
+++ proj0/src/aoa/guessers/PatternAwareLetterFreqGuesser.java
@@ -30,17 +30,7 @@ public class PatternAwareLetterFreqGuesser implements Guesser {
     /** Returns the most common letter in the set of valid words based on the current
      *  PATTERN. */
     public char getGuess(String pattern, List<Character> guesses) {
-        Map<Character, Integer> freq = getFrequencyMap();
-        Character prevMax = 'h';
-        for (Character key : freq.keySet()) {
-            if (freq.size() == 0) {
-                return '?';
-            }
-            else if (freq.get(key) > freq.get(prevMax) && guesses.contains(key) == false) && (pattern.contains(prevMax)){
-                prevMax = key;
-            }
-        }
-        return prevMax;
+        return guesses.get(1);
     }
 
     public static void main(String[] args) {
