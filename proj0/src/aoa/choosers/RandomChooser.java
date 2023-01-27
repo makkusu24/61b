32dce7869db2e982f71e8bb7ea733459d2c6c8f4
Max Boston <maxboston@berkeley.edu>
2023-01-26 21:20:42 -0800

    HOME - resonance

--- proj0/src/aoa/choosers/RandomChooser.java
+++ proj0/src/aoa/choosers/RandomChooser.java
@@ -3,30 +3,42 @@ package aoa.choosers;
 import edu.princeton.cs.algs4.StdRandom;
 import aoa.utils.FileUtils;
 
+import java.util.List;
+
+import static aoa.utils.FileUtils.readWordsOfLength;
+
 public class RandomChooser implements Chooser {
     private final String chosenWord;
     private String pattern;
 
     public RandomChooser(int wordLength, String dictionaryFile) {
-        // TODO: Fill in/change this constructor.
-        chosenWord = "";
+        List<String> words = readWordsOfLength(dictionaryFile, wordLength);
+        if (wordLength < 1) { //wordLength has to be a natural number
+            throw new IllegalArgumentException("wordLength cannot be less than 1");
+        }
+        if (readWordsOfLength(dictionaryFile, wordLength).size() == 0) { //no words in dictionary that match wordLength
+            throw new IllegalStateException("no words found of wordLength");
+        }
+        int numWords = words.size();
+        int randomlyChosenWordNumber = StdRandom.uniform(numWords);
+        String word = words.get(randomlyChosenWordNumber);
+        chosenWord = word;
     }
 
     @Override
-    public int makeGuess(char letter) {
+    public int makeGuess(char letter) { //returns the # occurrences of input letter in the secret word ('letter' is valid, lowercase, and novel)
         // TODO: Fill in this method.
         return 0;
     }
 
     @Override
-    public String getPattern() {
+    public String getPattern() { //return current game state pattern --> match correctly guessed letters to secret word letters, and unguessed letters to '-'; note: no leading or trailing spaces
         // TODO: Fill in this method.
         return "";
     }
 
     @Override
-    public String getWord() {
-        // TODO: Fill in this method.
+    public String getWord() { //This method should return the secret word being considered by the RandomChooser [14-26]
         return "";
     }
 }
