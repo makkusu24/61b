f32c5f919ad708886525c3317555972653fe2892
Max Boston <maxboston@berkeley.edu>
2023-01-30 16:13:20 -0800

    setup complete, yet to implement constructor

---
+++ proj1a/src/LinkedListDeque.java
@@ -0,0 +1,57 @@
+import java.util.List;
+
+public class LinkedListDeque<T> implements Deque<T> {
+    public static void main(String[] args) {
+        Deque<Integer> lld = new LinkedListDeque<>();
+    }
+
+    public LinkedListDeque() {
+        
+    }
+
+    @Override
+    public void addFirst(T x) {
+
+    }
+
+    @Override
+    public void addLast(T x) {
+
+    }
+
+    @Override
+    public List<T> toList() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+
+}
