e029c1c8eab4e6e71633f2be2aad9dc1390a03c9
Max Boston <maxboston@berkeley.edu>
2023-02-12 16:11:30 -0800

    fixes to be added

--- proj1b/src/ArrayDeque.java
+++ proj1b/src/ArrayDeque.java
@@ -5,33 +5,38 @@ public class ArrayDeque<T> implements Deque<T> {
 
     private T[] items;
     private int size;
+    private int front;
+    private int back;
 
     public ArrayDeque() {
-        T[] items = (T[]) new Object[8];
+        items = (T[]) new Object[8];
         size = 0;
+        front = 0;
+        back = -1; // specify size - 1 instead?
     }
 
-    /**
-     * @source https://docs.google.com/presentation/d/1kjbO8X7-i63NwQ_9wIt4HXr6APp2qc9PkghD-GO7_is/edit#slide=id.g1094ff4355_0_466
-     * @param args
-     * NOTE: multiplicative resizing up for time efficiency; resize down when <1/2 || <1/4 boxes used?
-     */
-    public static void main(String[] args) { //starting size 8
+    public static void main(String[] args) {
         Deque<Integer> ad = new ArrayDeque<>();
-        System.out.println(ad);
+        ad.addFirst(1);
+        ad.addFirst(0);
+        System.out.println(ad.toList());
     }
 
     private void resize(int desiredSize) {
         T[] sized = (T[]) new Object[desiredSize];
         System.arraycopy(items, 0, sized, 0, size);
         items = sized;
+        front = 0;
+        back = size - 1;
     }
 
     @Override
-    public void addFirst(T x) { //constant time implementation
+    public void addFirst(T x) {
         if (size == items.length) {
             resize(size * 2);
         }
+        front = (front - 1 + items.length) % items.length; // add conditional for if it's empty?
+        items[front] = x;
         size += 1;
     }
 
@@ -40,19 +45,23 @@ public class ArrayDeque<T> implements Deque<T> {
         if (size == items.length) {
             resize(size * 2);
         }
-        items[size] = x;
+        back = (back + 1) % items.length;
+        items[back] = x;
         size += 1;
     }
 
     @Override
     public List<T> toList() {
         List<T> returnList = new ArrayList<>();
+        for (int i = 0; i < size; i++) { // shouldn't start at 0?
+            returnList.add(items[i]);
+        }
         return returnList;
     }
 
     @Override
     public boolean isEmpty() {
-        if (items.length == 0) {
+        if (size == 0) {
             return true;
         }
         return false;
@@ -64,17 +73,32 @@ public class ArrayDeque<T> implements Deque<T> {
     }
 
     @Override
-    public T removeFirst() { // NULL out deleted items to prevent memory loitering (set return item -> delete original pointer)
-
+    public T removeFirst() {
+        if (isEmpty() == true) {
+            return null;
+        }
+        T removed = items[front];
+        items[front] = null;
+        front = (front + 1) % items.length;
         size -= 1;
-        return null;
+        if (size < 0 && size == items.length / 4) {
+            resize(items.length / 2);
+        }
+        return removed;
     }
 
     @Override
     public T removeLast() {
-        T removed = items[size - 1];
-        items[size - 1] = null;
+        if (isEmpty() == true) {
+            return null;
+        }
+        T removed = items[back];
+        items[back] = null;
+        back = (back - 1 + items.length) % items.length;
         size -= 1;
+        if (size < 0 && size == items.length / 4) {
+            resize(items.length / 2);
+        }
         return removed;
     }
 
