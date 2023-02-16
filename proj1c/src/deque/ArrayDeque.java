5b283d0e09ee55f1c81604bd6ff08c11a80357d8
Max Boston <maxboston@berkeley.edu>
2023-02-15 17:51:27 -0800

    added prev code

--- proj1c/src/deque/ArrayDeque.java
+++ proj1c/src/deque/ArrayDeque.java
@@ -1,5 +1,189 @@
 package deque;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayDeque<T> implements Deque<T> {
+
+    private T[] items; // array of generics
+    private int size; // cache with each operation
+    private int front; // keeps track of start of elements
+    private int back; // keeps track of end of elements
+    private static final int DEFAULT_SIZE = 8; // back array = 8 by default
+
+    /**
+     * Constructor for every new instance of an ArrayDeque.
+     * Items is type casted as a generic.
+     * Size is 0 for an empty array.
+     * Front is at the beginning of the array.
+     * Back can be -1 because the indexing is circular.
+     *
+     * @return newly instantiated ArrayDeque.
+     */
+    public ArrayDeque() {
+        items = (T[]) new Object[DEFAULT_SIZE];
+        size = 0;
+        front = 0;
+        back = (-1 + items.length) % items.length; // distinguish empty from non-empty ADs
+    }
+
+    public static void main(String[] args) {
+        Deque<Integer> ad1 = new ArrayDeque<>();
+        System.out.println(ad1.toList());
+    }
+
+    /**
+     * Creates a new array with a larger/smaller size and identical elements.
+     *
+     * @param desiredSize input can be additive or geometric.
+     * @return new ArrayDeque copy
+     */
+    private void resize(int desiredSize) {
+        T[] sized = (T[]) new Object[desiredSize];
+        for (int i = 0; i < size; i++) {
+            int index = (front + i) % items.length;
+            sized[i] = items[index];
+        }
+        items = sized;
+        front = 0;
+        back = size - 1;
+    }
+
+    /**
+     * Calls resize if adding would exceed instantiated size.
+     *
+     * @param x item to add in front of previous front pointer.
+     * @return altered ArrayDeque.
+     */
+    @Override
+    public void addFirst(T x) {
+        if (size == items.length) {
+            resize(size * 2);
+        }
+        front = (front - 1 + items.length) % items.length;
+        items[front] = x;
+        size += 1;
+    }
+
+    /**
+     * Calls resize if adding would exceed instantiated size.
+     *
+     * @param x item to add behind the previous back pointer.
+     * @return altered ArrayDeque.
+     */
+    @Override
+    public void addLast(T x) {
+        if (size == items.length) {
+            resize(size * 2);
+        }
+        back = (back + 1) % items.length;
+        items[back] = x;
+        size += 1;
+    }
+
+    /**
+     * Starts adding from front pointer.
+     *
+     * @return ArrayList representation of non-null elements in ArrayDeque.
+     */
+    @Override
+    public List<T> toList() {
+        List<T> returnList = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            int index = (front + i) % items.length;
+            if (items[index] != null) {
+                returnList.add(items[index]);
+            }
+        }
+        return returnList;
+    }
+
+    /**
+     * Regardless of length, returns empty if no elements.
+     *
+     * @return true or false.
+     */
+    @Override
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * All add and remove operations cache size of every ArrayDeque.
+     *
+     * @return positive integer of non-null element count.
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    /**
+     * Removes element at whatever index front pointer is at.
+     *
+     * @return altered ArrayDeque.
+     */
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        T removed = items[front];
+        items[front] = null; // removes pointer to save memory
+        front = (front + 1) % items.length;
+        size -= 1;
+        if (size == 0) {
+            front = 0;
+            back = (-1 + items.length) % items.length;
+        } else if (size < items.length / 4) {
+            resize(items.length / 2);
+        }
+        return removed;
+    }
+
+    /**
+     * Removes element at whatever index back pointer is at.
+     *
+     * @return altered ArrayDeque.
+     */
+    @Override
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+        T removed = items[(back) % items.length];
+        items[(back) % items.length] = null; // removes pointer to save memory
+        back = (back - 1 + items.length) % items.length;
+        size -= 1;
+        if (size == 0) {
+            front = 0;
+            back = (-1 + items.length) % items.length;
+        } else if (size < items.length / 4) {
+            resize(items.length / 2);
+        }
+        return removed;
+    }
+
+    /**
+     * Counts index starting from the front pointer.
+     *
+     * @param index non-null element index.
+     * @return null if out of bounds or negative. Element at index otherwise.
+     */
+    @Override
+    public T get(int index) {
+        if (index < 0 || index > size) {
+            return null;
+        }
+        return items[(front + index) % items.length];
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return get(index);
+    }
 
 }
--- proj1c/src/deque/LinkedListDeque.java
+++ proj1c/src/deque/LinkedListDeque.java
@@ -1,5 +1,155 @@
 package deque;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LinkedListDeque<T> implements Deque<T> {
+
+    private class Node { //each Node has its generic item, a next pointer, and a previous pointer
+        private T item;
+        private Node next;
+        private Node prev;
+
+        private Node(T i, Node n, Node p) {
+            item = i;
+            next = n;
+            prev = p;
+        }
+    }
+
+    private Node sentinel;
+    private int size;
+
+    /**
+     * @source [insert links for whatever inspiration you use for constructor
+     * LinkedListDeque() must take 0 args; keep as is
+     */
+    public LinkedListDeque() { // LLD constructor to add sentinel; points at itself topology
+        sentinel = new Node(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+        size = 0;
+
+    }
+    public static void main(String[] args) {
+        Deque<Integer> lld = new LinkedListDeque<>();
+        lld.addFirst(5);
+        lld.addFirst(4);
+        lld.addFirst(3);
+        lld.addFirst(2);
+        lld.addFirst(1);
+        lld.removeFirst();
+        /**
+         lld.addLast(5);
+         lld.addLast(4);
+         lld.addLast(3);
+         lld.addFirst(2);
+         lld.addLast(1);
+         lld.removeLast();
+         lld.removeLast();
+         */
+        /**
+         lld.addFirst("back");
+         lld.addFirst("middle");
+         lld.addFirst("front");
+         */
+        System.out.println(lld.toList());
+    }
+
+    @Override
+    public void addFirst(T x) {
+        Node first = new Node(x, sentinel.next, sentinel);
+        sentinel.next.prev = first;
+        sentinel.next = first;
+        size += 1;
+    }
+
+    @Override
+    public void addLast(T x) {
+        Node last = new Node(x, sentinel, sentinel.prev);
+        sentinel.prev.next = last;
+        sentinel.prev = last;
+        size += 1;
+    }
+
+    @Override
+    public List<T> toList() {
+        List<T> returnList = new ArrayList<>();
+        Node temp = sentinel.next;
+        while (temp != sentinel) {
+            returnList.add(temp.item);
+            temp = temp.next;
+        }
+        return returnList;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (sentinel.next == sentinel) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public T removeFirst() {
+        if (this.isEmpty()) {
+            return null;
+        } else {
+            T temp = sentinel.next.item;
+            sentinel.next = sentinel.next.next;
+            sentinel.next.prev = sentinel;
+            size -= 1;
+            return temp;
+        }
+    }
+
+    @Override
+    public T removeLast() {
+        if (this.isEmpty()) {
+            return null;
+        } else {
+            T temp = sentinel.prev.item;
+            sentinel.prev = sentinel.prev.prev;
+            sentinel.prev.next = sentinel;
+            size -= 1;
+            return temp;
+        }
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index > size) {
+            return null;
+        }
+        Node temp = sentinel.next;
+        int count = 0;
+        while (count != index) {
+            count += 1;
+            temp = temp.next;
+        }
+        return temp.item;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        if (index < 0 || index > size) {
+            return null;
+        } else {
+            return getRecursiveHelper(index, sentinel.next);
+        }
+    }
+
+    private T getRecursiveHelper(int index, Node s) {
+        if (index == 0) {
+            return s.item;
+        }
+        return getRecursiveHelper(index - 1, s.next);
+    }
 
 }
