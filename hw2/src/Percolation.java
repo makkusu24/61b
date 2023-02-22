80c0b102d9df762ec6b303fd25c1715f77adc7ce
Max Boston <maxboston@berkeley.edu>
2023-02-21 23:46:40 -0800

    before

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -17,42 +17,36 @@ public class Percolation {
         this.length = N * N + 3;
         this.topSite = length - 3;
         this.bottomSite = length - 2;
-        this.open = length - 1;
+        this.open = length - 1; // open connects to topSite -> game over
         this.openCount = 0;
         this.dimension = N;
-        /**
-        for (int i = 0; i < N; i++) { // connect top row to top site
-            uf.union(i, topSite);
-        }
-        for (int i = length - 4; i > length - 4 - N; i--) { // connect bottom row to bottom site
-            uf.union(i, bottomSite);
-        }
-         */
     }
 
     public void open(int row, int col) {
         int p = xyTo1D(row, col);
         validate(p);
+        if (!isOpen(row, col)) {
+            openCount += 1;
+        }
         uf.union(p, open);
         if (row == 0) { // add top row to top site
             uf.union(p, topSite);
         }
-        if (row != 0 && isOpen(row - 1, col)) {
+        if (row != 0 && isOpen(row - 1, col)) { // not top row
             uf.union(p, xyTo1D(row - 1, col));
         }
-        if (row != (dimension - 1) && isOpen(row + 1, col)) {
+        if (row != (dimension - 1) && isOpen(row + 1, col)) { // not bottom row
             uf.union(p, xyTo1D(row + 1, col));
         }
         if (row == dimension - 1) { // add bottom row to bottom site
             uf.union(p, bottomSite);
         }
-        if (col != 0 && isOpen(row, col - 1)) {
+        if (col != 0 && isOpen(row, col - 1)) { // not leftmost
             uf.union(p, xyTo1D(row, col - 1));
         }
-        if (col != (dimension - 1) && isOpen(row, col + 1)) {
+        if (col != (dimension - 1) && isOpen(row, col + 1)) { // not rightmost
             uf.union(p, xyTo1D(row, col + 1));
         }
-        openCount += 1;
     }
 
     public boolean isOpen(int row, int col) {
@@ -68,6 +62,7 @@ public class Percolation {
         int p = xyTo1D(row, col);
         validate(p);
         return uf.connected(p, topSite);
+        // Prevent backwash (connected to bottom but not top
     }
 
     public int numberOfOpenSites() {
