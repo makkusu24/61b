33c2b4b1b125076ee70291c8e876a25467b26f37
Max Boston <maxboston@berkeley.edu>
2023-02-21 22:39:14 -0800

    percolation imp 1

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -1,38 +1,87 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-// TODO: Add any other necessary imports.
 
 public class Percolation {
-    // TODO: Add any necessary instance variables.
+    WeightedQuickUnionUF uf;
+    private int length;
+    private int topSite;
+    private int bottomSite;
+    private int open;
+    private int openCount;
+    private int dimension;
 
     public Percolation(int N) {
-        // TODO: Fill in this constructor.
+        if (N <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        this.uf = new WeightedQuickUnionUF(N * N + 3);
+        this.length = N * N + 3;
+        this.topSite = length - 3;
+        this.bottomSite = length - 2;
+        this.open = length - 1;
+        this.openCount = 0;
+        this.dimension = N;
+        for (int i = 0; i < N; i++) { // connect top row to top site
+            uf.union(i, topSite);
+        }
+        for (int i = length - 4; i > length - 4 - N; i--) { // connect bottom row to bottom site
+            uf.union(i, bottomSite);
+        }
+    }
+
+    public static void main(String[] args) {
+        Percolation perc = new Percolation(5);
+        Percolation perc1 = new Percolation(9);
     }
 
     public void open(int row, int col) {
-        // TODO: Fill in this method.
+        int p = xyTo1D(row, col);
+        validate(p);
+        uf.union(p, open);
+        if (row != 0) {
+            uf.union(p, xyTo1D(row - 1, col));
+        }
+        if (row != dimension - 1) {
+            uf.union(p, xyTo1D(row + 1, col));
+        }
+        if (col != 0) {
+            uf.union(p, xyTo1D(row, col - 1));
+        }
+        if (col != dimension - 1) {
+            uf.union(p, xyTo1D(row, col + 1));
+        }
     }
 
     public boolean isOpen(int row, int col) {
-        // TODO: Fill in this method.
+        int p = xyTo1D(row, col);
+        validate(p);
+        if (uf.connected(p, open)) {
+            return true;
+        }
         return false;
     }
 
     public boolean isFull(int row, int col) {
-        // TODO: Fill in this method.
-        return false;
+        int p = xyTo1D(row, col);
+        validate(p);
+        return uf.connected(p, topSite);
     }
 
     public int numberOfOpenSites() {
-        // TODO: Fill in this method.
-        return 0;
+        return openCount;
     }
 
     public boolean percolates() {
-        // TODO: Fill in this method.
-        return false;
+        return uf.connected(topSite, bottomSite);
     }
 
-    // TODO: Add any useful helper methods (we highly recommend this!).
-    // TODO: Remove all TODO comments before submitting.
+    public int xyTo1D(int r, int c) {
+        return ((r * (r + 1)) - 1) + (c + 1);
+    }
+
+    private void validate(int p) {
+        if (p < 0 || p >= length - 3) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+    }
 
 }
