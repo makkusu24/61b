bd62f257d1b2f5bcf95806db20bbeb499bba1cd2
Max Boston <maxboston@berkeley.edu>
2023-03-01 07:43:26 -0800

    princeton documentation

--- lab07/src/BSTMap.java
+++ lab07/src/BSTMap.java
@@ -2,29 +2,93 @@ import java.util.Iterator;
 import java.util.Set;
 
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+
+    private Node root;
+
+    private class Node {
+        private K key;
+        private V value;
+        private Node left, right;
+        private int size;
+
+        public Node(K key, V value, int size) {
+            this.key = key;
+            this.value = value;
+            this.size = size;
+        }
+
+    }
+
     @Override
     public void put(K key, V value) {
+        if (key == null) {
+            throw new IllegalArgumentException();
+        }
+        root = put(root, key, value);
+    }
 
+    private Node put(Node x, K key, V value) {
+        if (x == null) {
+            return new Node (key, value, 1);
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = put(x.left, key, value);
+        } else if (cmp >0) {
+            x.right = put(x.right, key, value);
+        } else {
+            x.value = value;
+        }
+        x.size = 1 + size(x.left) + size(x.right);
+        return x;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return get(root, key);
+    }
+
+    private V get(Node x, K key) {
+        if (key == null) {
+            throw new IllegalArgumentException();
+        }
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return get(x.left, key);
+        } else if (cmp > 0) {
+            return get(x.right, key);
+        } else {
+            return x.value;
+        }
     }
 
     @Override
     public boolean containsKey(K key) {
-        return false;
+        if (key == null) {
+            throw new IllegalArgumentException();
+        }
+        return get(key) != null;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size(root);
+    }
+
+    private int size(Node x) {
+        if (x == null) {
+            return 0;
+        }
+        return x.size;
     }
 
     @Override
     public void clear() {
-
+        this.root = null;
+        this.root.size = 0;
     }
 
     @Override
