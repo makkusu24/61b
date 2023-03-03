0e5cb4ea841979e386ab9dd7d591616597c98822
Max Boston <maxboston@berkeley.edu>
2023-03-02 16:26:33 -0800

    historytext completed

---
+++ proj2a/ngordnet/main/HistoryTextHandler.java
@@ -0,0 +1,31 @@
+package ngordnet.main;
+
+import ngordnet.browser.NgordnetQuery;
+import ngordnet.browser.NgordnetQueryHandler;
+import ngordnet.ngrams.NGramMap;
+
+import java.util.List;
+
+public class HistoryTextHandler extends NgordnetQueryHandler {
+
+    NGramMap map;
+
+    public HistoryTextHandler(NGramMap ngram) {
+        this.map = ngram;
+    }
+    @Override
+    public String handle(NgordnetQuery q) {
+        List<String> words = q.words();
+        int startYear = q.startYear();
+        int endYear = q.endYear();
+
+        String response = "";
+        for (String word : words) {
+            response += word + ":";
+            response += map.weightHistory(word, startYear, endYear).toString();
+            response += "\n";
+        }
+
+        return response;
+    }
+}
