45ff8b220b600772b31fefe79ed041f2be4229e8
Max Boston <maxboston@berkeley.edu>
2023-03-02 16:34:40 -0800

    completion historytext, history

--- proj2a/ngordnet/main/HistoryHandler.java
+++ proj2a/ngordnet/main/HistoryHandler.java
@@ -1,4 +1,42 @@
 package ngordnet.main;
 
-public class HistoryHandler {
+import ngordnet.browser.NgordnetQuery;
+import ngordnet.browser.NgordnetQueryHandler;
+import ngordnet.ngrams.NGramMap;
+import ngordnet.ngrams.TimeSeries;
+import ngordnet.plotting.Plotter;
+import org.knowm.xchart.XYChart;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HistoryHandler extends NgordnetQueryHandler {
+
+    NGramMap map;
+
+    public HistoryHandler(NGramMap ngram) {
+        this.map = ngram;
+    }
+    @Override
+    public String handle(NgordnetQuery q) {
+        List<String> words = q.words();
+        int startYear = q.startYear();
+        int endYear = q.endYear();
+
+        ArrayList<TimeSeries> lts = new ArrayList<>();
+        ArrayList<String> labels = new ArrayList<>();
+
+        for (String word : words) {
+            lts.add(map.weightHistory(word, startYear, endYear));
+        }
+
+        for (String word : words) {
+            labels.add(word);
+        }
+
+        XYChart chart = Plotter.generateTimeSeriesChart(labels, lts);
+        String encodedImage = Plotter.encodeChartAsString(chart);
+
+        return encodedImage;
+    }
 }
