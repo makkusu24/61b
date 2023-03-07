8724bab553c42be1f676dff2a11899ff1354dbc1
Max Boston <maxboston@berkeley.edu>
2023-03-07 13:56:08 -0800

    slight progress

--- lab08/src/hashmap/MyHashMap.java
+++ lab08/src/hashmap/MyHashMap.java
@@ -3,6 +3,11 @@ package hashmap;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.ArrayDeque;
+import java.util.Stack;
+import java.util.HashSet;
 
 /**
  *  A hash table-backed Map implementation. Provides amortized constant time
@@ -12,6 +17,94 @@ import java.util.Set;
  *  @author YOUR NAME HERE
  */
 public class MyHashMap<K, V> implements Map61B<K, V> {
+    /**
+     * Protected helper class to store key/value pairs
+     * The protected qualifier allows subclass access
+     */
+    protected class Node {
+        K key;
+        V value;
+
+        Node(K k, V v) {
+            key = k;
+            value = v;
+        }
+    }
+
+    /* Instance Variables */
+    private Collection<Node>[] buckets; // Collection supports add, remove, iterator, .equal() to desired key
+    private double loadFactor;
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final double DEFAULT_LOADFACTOR = 0.75;
+
+    /** Constructors */
+    public MyHashMap() {
+        new MyHashMap(DEFAULT_CAPACITY, DEFAULT_LOADFACTOR);
+    }
+
+    public MyHashMap(int initialCapacity) {
+        new MyHashMap(initialCapacity, DEFAULT_LOADFACTOR);
+    }
+
+    /**
+     * MyHashMap constructor that creates a backing array of initialCapacity.
+     * The load factor (# items / # buckets) should always be <= loadFactor
+     *
+     * @param initialCapacity initial size of backing array
+     * @param loadFactor maximum load factor
+     */
+    public MyHashMap(int initialCapacity, double loadFactor) {
+        if (initialCapacity < 1 || loadFactor <= 0.0) {
+            throw new IllegalArgumentException();
+        }
+        this.buckets = createTable(initialCapacity);
+        this.loadFactor = loadFactor;
+    }
+
+    /**
+     * Returns a new node to be placed in a hash table bucket
+     */
+    private Node createNode(K key, V value) {
+        return new Node(key, value);
+    }
+
+    /**
+     * Returns a data structure to be a hash table bucket
+     *
+     * The only requirements of a hash table bucket are that we can:
+     *  1. Insert items (`add` method)
+     *  2. Remove items (`remove` method)
+     *  3. Iterate through items (`iterator` method)
+     *
+     * Each of these methods is supported by java.util.Collection,
+     * Most data structures in Java inherit from Collection, so we
+     * can use almost any data structure as our buckets.
+     *
+     * Override this method to use different data structures as
+     * the underlying bucket type
+     *
+     * BE SURE TO CALL THIS FACTORY METHOD INSTEAD OF CREATING YOUR
+     * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
+     */
+    protected Collection<Node> createBucket() {
+        return new LinkedList<>();
+    }
+
+    /**
+     * Returns a table to back our hash table. As per the comment
+     * above, this table can be an array of Collection objects
+     *
+     * BE SURE TO CALL THIS FACTORY METHOD WHEN CREATING A TABLE SO
+     * THAT ALL BUCKET TYPES ARE OF JAVA.UTIL.COLLECTION
+     *
+     * @param tableSize the size of the table to create
+     */
+    private Collection<Node>[] createTable(int tableSize) {
+        return null;
+    }
+
+    // Map 61B Interface implementation below
+    // Your code won't compile until you do so!
 
     @Override
     public void put(K key, V value) {
@@ -52,82 +145,5 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     public Iterator<K> iterator() {
         throw new UnsupportedOperationException();
     }
-
-    /**
-     * Protected helper class to store key/value pairs
-     * The protected qualifier allows subclass access
-     */
-    protected class Node {
-        K key;
-        V value;
-
-        Node(K k, V v) {
-            key = k;
-            value = v;
-        }
-    }
-
-    /* Instance Variables */
-    private Collection<Node>[] buckets;
-    // You should probably define some more!
-
-    /** Constructors */
-    public MyHashMap() { }
-
-    public MyHashMap(int initialCapacity) { }
-
-    /**
-     * MyHashMap constructor that creates a backing array of initialCapacity.
-     * The load factor (# items / # buckets) should always be <= loadFactor
-     *
-     * @param initialCapacity initial size of backing array
-     * @param loadFactor maximum load factor
-     */
-    public MyHashMap(int initialCapacity, double loadFactor) { }
-
-    /**
-     * Returns a new node to be placed in a hash table bucket
-     */
-    private Node createNode(K key, V value) {
-        return null;
-    }
-
-    /**
-     * Returns a data structure to be a hash table bucket
-     *
-     * The only requirements of a hash table bucket are that we can:
-     *  1. Insert items (`add` method)
-     *  2. Remove items (`remove` method)
-     *  3. Iterate through items (`iterator` method)
-     *
-     * Each of these methods is supported by java.util.Collection,
-     * Most data structures in Java inherit from Collection, so we
-     * can use almost any data structure as our buckets.
-     *
-     * Override this method to use different data structures as
-     * the underlying bucket type
-     *
-     * BE SURE TO CALL THIS FACTORY METHOD INSTEAD OF CREATING YOUR
-     * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
-     */
-    protected Collection<Node> createBucket() {
-        return null;
-    }
-
-    /**
-     * Returns a table to back our hash table. As per the comment
-     * above, this table can be an array of Collection objects
-     *
-     * BE SURE TO CALL THIS FACTORY METHOD WHEN CREATING A TABLE SO
-     * THAT ALL BUCKET TYPES ARE OF JAVA.UTIL.COLLECTION
-     *
-     * @param tableSize the size of the table to create
-     */
-    private Collection<Node>[] createTable(int tableSize) {
-        return null;
-    }
-
-    // TODO: Implement the methods of the Map61B Interface below
-    // Your code won't compile until you do so!
 
 }
